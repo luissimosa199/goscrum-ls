566537dca5f2c7977c57fd24db6f9370e82fa5ae
Luis Simosa <simosa37@gmail.com>
2022-05-15 15:31:01 -0300

    Updates

---
+++ static/js/308.a788e56a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoscrum_ls=self.webpackChunkgoscrum_ls||[]).push([[308],{2308:function(n,s,c){c.r(s);var e=c(184);s.default=function(){return(0,e.jsx)("div",{className:"container",children:"P\xe1gina no encontrada"})}}}]);
+//# sourceMappingURL=308.a788e56a.chunk.js.map
